d4ea58afe3a1a20a0f53bc2416cbd1d6349a29ff
Joseph <jdirayoussef1997@gmail.com>
2026-04-22 22:43:01 +0100

    migration

---
+++ database/migrations/2026_04_22_213217_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->string('title', 255);
+            $table->text('content');
+            $table->enum('status', ['draft', 'published']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
