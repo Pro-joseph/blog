8f8ea05aef55b1b525631e7111b14df1126e93f1
Joseph <jdirayoussef1997@gmail.com>
2026-04-22 22:16:12 +0100

    UI

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('public_blog_home');
 });
+
+Route::get('/login', function () {
+    return view('admin_login');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('admin_dashboard');
+});
+
+
+Route::get('/create', function () {
+    return view('create_article');
+});
+
+Route::get('/edit', function () {
+    return view('edit_article');
+});
+
+
+Route::get('/article', function () {
+    return view('article_detail');
+});
+
